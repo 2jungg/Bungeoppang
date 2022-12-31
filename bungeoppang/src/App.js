eb6335e336f727b1daf279e22d5066923ddac604
2jungg <2jungg@kaist.ac.kr>
2023-01-01 06:33:14 +0900

    pos button installed

--- bungeoppang/src/App.js
+++ bungeoppang/src/App.js
@@ -1,59 +1,72 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const { kakao } = window;
 
+/* 나중에 혹시 쓸 수도 있을 가게 위치들 */
 class store extends React.Component {
   constructor(props) {
     super(props);
   }
 }
 
-function addClick() {
-  console.log('add is clicked!');
+function MakeMap(Lat, Lng, level) {
+  const container = document.getElementById('map');
+  const options = {
+    center: new kakao.maps.LatLng(Lat, Lng),
+    level: level,
+  };
+  const map = new kakao.maps.Map(container, options);
 }
 
-function posClick() {
-  console.log('pos is clicked!');
-  if (navigator.geolocation) {
-    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var lat = position.coords.latitude, // 위도
-          lon = position.coords.longitude; // 경도
-      console.log(lat, lon);
-      // const map = new kakao.maps.Map(container, options);
-      // map.setCenter(new kakao.maps.LatLng(lat, lon))
-      });
-    
-  } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
-    alert("위치를 확인할 수 없습니다. GPS가 켜져 있는지 확인해 주세요.");
-  }
-}
-
+/* 지도 표시 컴포넌트 */
 function Map() {
   useEffect(() => {
-    const container = document.getElementById('map');
-    const options = {
-      center: new kakao.maps.LatLng(36.3504119, 127.3845475),
-      level: 3,
-    };
-    const map = new kakao.maps.Map(container, options);
-
+    MakeMap(36.3504119, 127.3845475, 3);
   }, [])
 
   return (
-    <div id="map"></div>
+    <div id="map">
+    </div>
   )
 }
 
+/* 최상위 컴포넌트 */
 function App() {
+  const [addWait, setaddWait] = useState(false);
+
+  const addClick = () => {
+    console.log('add is clicked!');
+    setaddWait(prevState => !prevState)
+  }
+
+  const posClick = () => {
+    console.log('pos is clicked!');
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude, // 위도
+            lon = position.coords.longitude; // 경도
+        console.log(lat, lon);
+        MakeMap(lat, lon, 3);
+        }, function(error) {
+          alert("위치를 확인할 수 없습니다. GPS가 켜져 있는지 확인해 주세요.");
+        });
+      
+    } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+      console.log("can't find locate!");
+      alert("위치를 확인할 수 없습니다. Geolocation을 지원하지 않습니다.");
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <Map />
         <button className="Button-add" onClick={addClick}> + </button>
         <button className="Button-pos" onClick={posClick}> pos </button>
+        {addWait? <div className="Add-wait"></div> : null}
       </header>
     </div>
   );
